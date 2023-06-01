51d5ea84096466b89af6c15a71d858b0fc20198f
jotafuentes <joseluisfuentes17@gmail.com>
2023-06-01 16:42:17 +0200

    implement ContactsPage as a stateful component

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,33 +3,60 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({contacts, addContact}) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
-
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [duplicate, setDuplicate] = useState(false)
+ 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if(!duplicate) {
+    addContact(name, phone, email);
+    setName('')
+    setPhone('')
+    setEmail('')
+   }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
-
+  useEffect(() => {
+    const isDuplicate = contacts.some((contact) => contact.name === name);
+    if(isDuplicate === false){
+      setDuplicate(false)
+    } else {
+      setDuplicate(true)
+    }
+  }, [contacts, name, duplicate])
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+        name={name}
+        setName={setName}
+        phone={phone}
+        setPhone={setPhone}
+        email={email}
+        setEmail={setEmail}
+        duplicate={duplicate}
+        handleSubmit={handleSubmit}/>
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts}/>
       </section>
     </div>
   );
