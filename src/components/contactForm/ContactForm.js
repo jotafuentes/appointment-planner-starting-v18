b53badd350a5b4784e27a555850a30cf5dcd5450
jotafuentes <joseluisfuentes17@gmail.com>
2023-06-02 20:24:21 +0200

    TileList component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,42 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}> 
+    <label>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => {setName(e.target.value)}}
+        placeholder="Contact Name"
+        required
+      />
+    </label>
+    <label>
+      <input
+        type="tel"
+        name="phone"
+        value={phone}
+        onChange={(e) => {setPhone(e.target.value)}}
+        //regex teléfono
+        pattern="^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$"
+        placeholder="Contact Phone Number"
+        required
+      />
+    </label>
+    <label>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => {setEmail(e.target.value)}}
+        placeholder="Contact Email"
+        required
+      />
+    </label>
+    <input name='Submit' type="submit" placeholder="Add Contact"/>
+      
+    </form>
   );
 };
 
